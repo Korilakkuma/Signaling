b25b043dbd8dc61cc2a847d1268ba1cf9d3e355d
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2020-12-21 17:27:12 +0900

    Refactor signaling server

--- signaling.js
+++ signaling.js
@@ -1,21 +1,19 @@
 'use strict';
 
-const WebSocketServer = require('ws').Server;
+const WebSocket = require('ws');
+const WebSocketServer = WebSocket.Server;
 const port = (process.argv[2] > 0) && (process.argv[2] <= 65535) ? process.argv[2] : 3000;
 
 const ws = new WebSocketServer({ port });
 
 console.log(`Wait port (${port}) ...`);
 
-const sockets = [];
-
 ws.on('connection', (socket) => {
-  sockets.push(socket);
-
   socket.on('message', (message) => {
     ws.clients.forEach((client) => {
-      if (socket !== client) {
+      if ((socket !== client) && (socket.readyState === WebSocket.OPEN)) {
         client.send(message);
+        console.log('--- Sent ---');
       } else {
         console.log('--- Skip Sender ---');
       }
